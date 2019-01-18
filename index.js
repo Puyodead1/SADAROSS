64192db60591642d205a810be8808c96264daa4a
Puyodead1 <23562356riley@gmail.com>
2019-01-17 21:18:01 -0500

    update

--- index.js
+++ index.js
@@ -74,6 +74,45 @@ client.on('message', async msg => {
         break
       /* eslint-enable */
     }
+  } else if (command === 'quit') {
+    if (
+      msg.author.id === '213247101314924545' ||
+      msg.author.id === '505466807092772865' ||
+      msg.author.id === '213247101314924545'
+    ) {
+      if (!args.length >= 1) {
+        let embed = new Discord.RichEmbed()
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setColor('#FF0000')
+          .setDescription(`Invalid Syntax`)
+          .addField(`Usage`, `s!quit <reason>`)
+          .setTimestamp()
+          .setFooter(
+            `SATAROSS by Puyodead1 and Puyodead1 Development`,
+            client.user.avatarURL
+          )
+        return msg.channel.send(embed)
+      }
+      let reason = args.join(' ')
+      await msg.guild.members
+        .get('213247101314924545')
+        .send(`${msg.author.username} shut me down for ${reason}`)
+      await msg.channel.send(
+        `System Shutdown for ${reason}. Incident has been reported.`
+      )
+      return process.exit(0)
+    } else {
+      let embed1 = new Discord.RichEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .setColor('#FF0000')
+        .setDescription(`You are not authorized to do that!`)
+        .setTimestamp()
+        .setFooter(
+          `SATAROSS by Puyodead1 and Puyodead1 Development`,
+          client.user.avatarURL
+        )
+      return msg.channel.send(embed1)
+    }
   }
 })
 
